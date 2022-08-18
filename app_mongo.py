8a436f1c4bf0a06454be79200536a616a7309d6e
Vincent Veri <vincent.veri@gmail.com>
2022-08-18 21:28:44 +0200

    create a basic mongo app

---
+++ app_mongo.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+if __name__ == "__main__":
+    mongo = MongoClient(host='localhost', port=27017)
+
+    # create a DB
+    db = mongo.learn
+    # get a reference to a collection
+    collection = db.student
+
+    '''Insert a new document
+    a_doc = {
+        'name': 'Peter Bishop',
+        'age': 34,
+        'interests': ['Universe', 'Science']
+    }
+    #collection.insert_one(a_doc)
+    '''
+
+    object_id = ObjectId("624df55ac805794c31a28ceb")
+    # collection.update_one({"_id": object_id}, {"$set": {'name': 'Maria Elena Oliva'}})
+    student = collection.find_one({"_id": object_id})
+    print(student)
