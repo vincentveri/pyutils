fa00fd38b773fd0817d036c407619c4c3571aba0
Vincent <vincent.veri@gmail.com>
2022-10-27 18:59:49 +0200

    create a binary search script

---
+++ algorithms/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(my_list, item):
+    low = 0
+    high = len(my_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = my_list[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9, 11, 13]
+
+print(binary_search(my_list, 5))
+print(binary_search(my_list, 2))
